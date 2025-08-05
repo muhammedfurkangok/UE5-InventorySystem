614b1e999c4fea7ac497ce2bf95a1d914da8f6c4
Muhammed Furkan Gok <gkkangk@gmail.com>
2025-08-05 23:11:26 +0300

    feat: ItemData Structs Included & Data Table Included

---
+++ Source/InventorySystem/Public/Data/ItemDataStructs.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ItemDataStructs.generated.h" // when you generate header files, this should be the last include
+
+UENUM()
+enum class EItemQuality : uint8 //normally enum use int8, but you can use uint8 for better performance
+{
+	Shoddy UMETA(DisplayName = "Shoddy"),
+	Common UMETA(DisplayName = "Common"),
+	Quality UMETA(DisplayName = "Quality"),
+	MasterWork UMETA(DisplayName = "Masterwork"),
+	Grandmaster UMETA(DisplayName = "Grandmaster"),
+};
+
+UENUM()
+enum class EItemType:uint8
+{
+	Armor UMETA(DisplayName = "Armor"),
+	Weapon UMETA(DisplayName = "Weapon"),
+	Shield UMETA(DisplayName = "Shield"),
+	Spell UMETA(DisplayName = "Spell"),
+	Consumable UMETA(DisplayName = "Consumable"),
+	Quest UMETA(DisplayName = "Quest"),
+	Mundane UMETA(DisplayName = "Mundane"),
+};
+
+USTRUCT()
+struct FItemStatistics
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	float ArmorRating;
+	UPROPERTY(EditAnywhere)
+	float DamageValue;
+	UPROPERTY(EditAnywhere)
+	float RestorationValue;
+	UPROPERTY(EditAnywhere)
+	float SellValue;
+};
+
+USTRUCT()
+struct FItemTextData
+{
+	GENERATED_BODY()
+
+
+	UPROPERTY(EditAnywhere)
+	FText ItemName;
+	UPROPERTY(EditAnywhere)
+	FText Description;
+	UPROPERTY(EditAnywhere)
+	FText InteractionText;
+	UPROPERTY(EditAnywhere)
+	FText UsageText;
+};
+
+USTRUCT()
+struct FItemNumericData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	float Weight;
+
+	UPROPERTY(EditAnywhere)
+	int32 MaxStackSize;
+
+	UPROPERTY(EditAnywhere)
+	bool bIsStackable;
+};
+
+USTRUCT()
+struct FItemAssetData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	UTexture2D* Icon;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMesh* Mesh;;
+};
+
+
+USTRUCT()
+struct FItemData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	FName ID;
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	EItemType ItemType;
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	EItemQuality ItemQuality;
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	FItemStatistics ItemStatistics;
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	FItemTextData ItemTextData;
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	FItemNumericData ItemNumericData;
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	FItemAssetData ItemAssetData;
+};
