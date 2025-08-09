a8c540afb750a9d3e823348659bc705add17b0f1
Muhammed Furkan Gok <gkkangk@gmail.com>
2025-08-09 16:47:58 +0300

    ref: Inventory System h. file refactored

--- Source/InventorySystem/Public/Player/InventorySystemCharacter.h
+++ Source/InventorySystem/Public/Player/InventorySystemCharacter.h
@@ -47,6 +47,10 @@ class AInventorySystemCharacter : public ACharacter
 public:
 	AInventorySystemCharacter();
 	
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
 protected:
 
@@ -56,18 +60,12 @@ protected:
 	/** Called for looking input */
 	void Look(const FInputActionValue& Value);
 			
-
-protected:
 	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	
 	// To add mapping context
 	virtual void BeginPlay();
 
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
 };
 
