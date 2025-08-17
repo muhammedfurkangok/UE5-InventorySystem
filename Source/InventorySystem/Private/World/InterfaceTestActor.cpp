660f9964c706f591a909ca61523aea810ad404b5
Muhammed Furkan Gok <gkkangk@gmail.com>
2025-08-17 23:14:18 +0300

    ref :add custom depth and logging to InterfaceTestActor

--- Source/InventorySystem/Private/World/InterfaceTestActor.cpp
+++ Source/InventorySystem/Private/World/InterfaceTestActor.cpp
@@ -6,51 +6,53 @@
 // Sets default values
 AInterfaceTestActor::AInterfaceTestActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 
 	SetRootComponent(Mesh);
-
 }
 
 // Called when the game starts or when spawned
 void AInterfaceTestActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AInterfaceTestActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 void AInterfaceTestActor::BeginFocus()
 {
-	if(Mesh)
+	if (Mesh)
 	{
-		
-	}	
-	
+		Mesh->SetRenderCustomDepth(true);
+	}
 }
 
 void AInterfaceTestActor::EndFocus()
 {
+	if (Mesh)
+	{
+		Mesh->SetRenderCustomDepth(false);
+	}
 }
 
 void AInterfaceTestActor::BeginInteract()
 {
+	UE_LOG(LogTemp, Warning, TEXT("Begin Interact with %s"), *GetName());
 }
 
 void AInterfaceTestActor::EndInteract()
 {
+	UE_LOG(LogTemp, Warning, TEXT("End Interact with %s"), *GetName());
 }
 
 void AInterfaceTestActor::Interact()
 {
+	UE_LOG(LogTemp, Warning, TEXT("Interact with %s"), *GetName());
 }
-
