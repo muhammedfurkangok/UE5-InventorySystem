b4c8db5e6eb337b62ac832f6b4e32f891b5f9f82
Muhammed Furkan Gok <gkkangk@gmail.com>
2026-01-21 23:52:06 +0300

    feat: Item Component Variables Set

--- Source/InventorySystem/Public/Components/InventoryComponent.h
+++ Source/InventorySystem/Public/Components/InventoryComponent.h
@@ -7,6 +7,66 @@
 #include "InventoryComponent.generated.h"
 
 
+UENUM(BlueprintType)
+enum class EItemAddResult:uint8
+{
+	IAR_NoItemAdded UMETA(DisplayName="No Item Added"),
+	IAR_PartialAmountItemAdded UMETA(DisplayName="Partial Amount of Item Added"),
+	IAR_AllItemAdded UMETA(DisplayName="All Items Added")
+};
+
+USTRUCT(BlueprintType)
+struct FItemAddResult
+{
+	GENERATED_BODY()
+
+	FItemAddResult() : ActualAmountAdded(0),
+	OperationResult(EItemAddResult::IAR_NoItemAdded),
+	ResultMessage(FText::GetEmpty())
+	{};
+
+	//Act amount of items that were actually added to the inventory
+	UPROPERTY(BlueprintReadOnly, Category= "Item Add Result")
+	int32 ActualAmountAdded;
+	//Enum represeting the end state off an add item operation
+	UPROPERTY(BlueprintReadOnly, Category= "Item Add Result")
+	EItemAddResult OperationResult;
+	//A message describing the result of the add item operation
+	UPROPERTY(BlueprintReadOnly ,Category= "Item Add Result")
+	FText ResultMessage;
+
+	static FItemAddResult AddedNone(const FText& ErrorText)
+	{
+		FItemAddResult AddedNoneResult;
+		AddedNoneResult.ActualAmountAdded = 0;
+		AddedNoneResult.OperationResult = EItemAddResult::IAR_NoItemAdded;
+		AddedNoneResult.ResultMessage = ErrorText;
+		return AddedNoneResult;
+	};
+	
+	static FItemAddResult AddedPartial(const int32 PartialAmountAdded, const FText& ErrorText)
+	{
+		FItemAddResult AddedPartialResult;
+		AddedPartialResult.ActualAmountAdded = PartialAmountAdded;
+		AddedPartialResult.OperationResult = EItemAddResult::IAR_PartialAmountItemAdded;
+		AddedPartialResult.ResultMessage = ErrorText;
+		return AddedPartialResult;
+	};
+	
+	static FItemAddResult AddedAll(const int32 AmountAdded, const FText& Message)
+	{
+		FItemAddResult AddedAllResult;
+		AddedAllResult.ActualAmountAdded = AmountAdded;
+		AddedAllResult.OperationResult = EItemAddResult::IAR_AllItemAdded;
+		AddedAllResult.ResultMessage = Message;
+		return AddedAllResult;
+	};
+};
+
+	
+	
+};
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class INVENTORYSYSTEM_API UInventoryComponent : public UActorComponent
 {
