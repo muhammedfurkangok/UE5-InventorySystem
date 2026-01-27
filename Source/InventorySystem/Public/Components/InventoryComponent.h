efe44f6939750424bfddec2e6d95d63d36df0053
Muhammed Furkan Gok <gkkangk@gmail.com>
2026-01-27 23:41:49 +0300

    feat: InventoryComponent declaration progress

--- Source/InventorySystem/Public/Components/InventoryComponent.h
+++ Source/InventorySystem/Public/Components/InventoryComponent.h
@@ -6,6 +6,8 @@
 #include "Components/ActorComponent.h"
 #include "InventoryComponent.generated.h"
 
+DECLARE_MULTICAST_DELEGATE(FOnInventoryUpdated);
+
 
 UENUM(BlueprintType)
 enum class EItemAddResult:uint8
@@ -34,7 +36,7 @@ struct FItemAddResult
 	//A message describing the result of the add item operation
 	UPROPERTY(BlueprintReadOnly ,Category= "Item Add Result")
 	FText ResultMessage;
-
+	
 	static FItemAddResult AddedNone(const FText& ErrorText)
 	{
 		FItemAddResult AddedNoneResult;
@@ -63,26 +65,56 @@ struct FItemAddResult
 	};
 };
 
-	
-	
-};
+
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class INVENTORYSYSTEM_API UInventoryComponent : public UActorComponent
 {
 	GENERATED_BODY()
+	
+public:
+	
+	FOnInventoryUpdated InventoryWasUpdated;
 
-public:	
-	// Sets default values for this component's properties
+	UPROPERTY(VisibleAnywhere, Category="Inventory")
+	float InventoryTotalWeight;
+	UPROPERTY(EditInstanceOnly, Category="Inventory")
+	int32 ItemSlotCount;
+	UPROPERTY(EditInstanceOnly, Category="Inventory")
+	float MaxWeightCapacity;
+
+	
 	UInventoryComponent();
 
+	FItemAddResult HandleAddItem(const TObjectPtr<UItemBase>& InputItem);
+	UItemBase* FindMatchingItem(const TObjectPtr<UItemBase>& ItemIn) const;
+	UItemBase* FindNextItemByID(const TObjectPtr<UItemBase>& ItemIn) const;
+	UItemBase* FindNextPartialStack(const TObjectPtr<UItemBase>& ItemIn) const;
+	
+	/** @brief Merges quantities of two items together that are already in the inventory.*/
+	void MergeItems(const TObjectPtr<UItemBase>& TargetItem, const TObjectPtr<UItemBase>& SourceItem);
+	void HandleRemoveItem(UItemBase* ItemToRemove, int32 AmountToRemove = 0, const bool bAdjustWeight = true);
+	
+	/** @brief Used to split an existing stack. Reduces the target stack quantity and adds the split amount as a new item.*/
+	void SplitExistingStack(UItemBase* ItemIn, const int32 AmountToSplit);
+	
+	FORCEINLINE TArray<UItemBase*> GetInventoryContents() const { return InventoryContents; }
+
+	
+	FORCEINLINE void SetSlotsCapacity(const int32 NewSlotsCapacity) { ItemSlotCount = NewSlotsCapacity; }
+	FORCEINLINE void SetWeightCapacity(const float NewWeightCapacity) { MaxWeightCapacity = NewWeightCapacity; }
+
 protected:
-	// Called when the game starts
+	UPROPERTY(VisibleAnywhere, Category="Inventory")
+	TArray<TObjectPtr<UItemBase>> InventoryContents;
+
+	
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	FItemAddResult HandleNonStackableItems(const TObjectPtr<UItemBase>& InputItem);
+	int32 HandleStackableItems(const TObjectPtr<UItemBase>& ItemIn, int32 RequestedAddAmount);
+	int32 CalculateWeightAddAmount(const TObjectPtr<UItemBase>& ItemIn, int32 RequestedAddAmount) const;
+	int32 CalculateNumberForFullStack(const TObjectPtr<UItemBase>& StackableItem, int32 RequestedAddAmount) const;
 
-		
+	void AddNewItem(const TObjectPtr<UItemBase>& Item, const int32 AmountToAdd);
 };
